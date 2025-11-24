31721533070f9ca2dc9c1e0be4150116c94947a8
Pedro Márcio Di Pietro <pedromarcioap@gmail.com>
2025-11-24 12:38:48 -0300

    refactor: Update CSP nonce for script imports
    Replaces the hardcoded CSP nonce in `index.html` and `manifest.json`.
    Removes deprecated `loader.js` file.

--- loader.js
+++ loader.js
@@ -1,2 +1,1 @@
-// Deprecated. Loader logic has been moved to index.html with a CSP nonce.
-// This file can be safely deleted or ignored.
+// File deprecated. Import map logic moved to index.html with nonce.
